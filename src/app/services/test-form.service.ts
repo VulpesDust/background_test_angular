653decc8d5f8559bb1b987f318afe5fdbeefafce
VulpesDust <noise13qwer@gmail.com>
2024-11-09 20:48:04 +0300

    update test-form.service

--- src/app/services/test-form.service.ts
+++ src/app/services/test-form.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {FormGroup} from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -7,12 +8,32 @@ import {HttpClient} from '@angular/common/http';
 export class TestFormService {
 
   private url: string = '/forms/';
+  private isBackendExists = false;
 
   constructor(private http: HttpClient) {
   }
 
-  public getForm(name: string) {
-    return this.http.get(this.url + name + '.json')
+  /**
+   * без настроенного бека тянет файл из public/forms/{formName}.json
+   */
+  public getForm(formName: string) {
+    const path = formName + (this.isBackendExists ? '' : '.json')
+    return this.http.get(this.url + path)
   }
 
+  /**
+   * без бекенда не работает
+   */
+  public submitForm(formGroup: FormGroup) {
+    console.log(JSON.stringify(formGroup.value)) // TODO DELETE ON BACKEND COMPLETE, сейчас нужно для заполнения результатов из консоли в файл для теста без бека
+    return this.http.post(this.url + 'submit', JSON.stringify(formGroup.value));
+  }
+
+  /**
+   * без настроенного бека тянет файл из public/forms/{formName}/{resultName}.json
+   */
+  public getSubmittedFormData(formName: string, resultName: string) {
+    const path = formName + '/' + resultName + (this.isBackendExists ? '' : '.json')
+    return this.http.get(this.url + path)
+  }
 }
