35c5d4e8b94fab159146b742464ce2ac29c33c51
VulpesDust <noise13qwer@gmail.com>
2024-11-09 21:01:45 +0300

    update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {MainComponent} from './components/main/main.component';
+import {TestFormComponent} from './components/test-form/test-form.component';
+import {TestingElementsComponent} from './components/testing-elements/testing-elements.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: MainComponent, title: 'main page'},
+  {path: 'test-form/:form', component: TestFormComponent, title: 'testForm'},
+  {path: 'test-base-elements', component: TestingElementsComponent, title: 'test base elements'},
+];
