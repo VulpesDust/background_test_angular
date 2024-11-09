37f5bef8c7040d35c6677eb89c90ddc4fabf3d55
VulpesDust <noise13qwer@gmail.com>
2024-11-09 20:52:10 +0300

    update test-number.component

--- src/app/components/test-number/test-number.component.ts
+++ src/app/components/test-number/test-number.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {TestFormElementComponent} from '../test-element/test-form-element.component';
 
 @Component({
   selector: 'app-test-number',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './test-number.component.html',
   styleUrl: './test-number.component.scss'
 })
-export class TestNumberComponent {
+export class TestNumberComponent extends TestFormElementComponent {
 
+  ngOnInit() {
+    if (!this.el['placeholder']) this.el['placeholder'] = '';
+
+    const formControl = new FormControl();
+    if (this.el['required']) {
+      formControl.setValidators(Validators.required)
+    }
+    this.formGroup.addControl(this.el['form-id'], formControl);
+  }
+
+  add(x: number) {
+    const name = this.el['form-id']
+    let value = this.formGroup.value[name]
+    if (value != null) {
+      value += x;
+    } else {
+      value = x;
+    }
+    this.formGroup.controls[name].setValue(value);
+  }
 }
